10061090f93107699a77ecb9d83a7d0bf8043a4b
Adel <htwr86@gmail.com>
2018-07-16 15:18:16 +0300

    change HABTM on has_many through

--- app/models/route.rb
+++ app/models/route.rb
@@ -1,5 +1,6 @@
-class Route < ActiveRecord::Base
-  has_and_belongs_to_many :railway_stations
+class Route < ApplicationRecord
+  has_many :railway_stations_routes
+  has_many :railway_stations, through: :railway_stations_routes
   has_many :trains
 	validates :title, presence: true
 end
