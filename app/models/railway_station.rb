d15cd9499aa3ca50e1cc25b95af0eeda2d699d71
Adel <htwr86@gmail.com>
2018-08-08 02:16:12 +0300

    scope r-station problem

--- app/models/railway_station.rb
+++ app/models/railway_station.rb
@@ -5,4 +5,17 @@ class RailwayStation < ApplicationRecord
   has_many :trains, foreign_key: :current_station_id
   
   validates :title, presence: true
+
+  scope :ordered, -> { joins(:railway_stations_routes).order("railway_stations_routes.position") }
+
+  def update_position(route, position)
+    rsr = railway_stations_routes.where(route: route).first
+    rsr.update(position: position) if rsr
+  end
+
+  protected
+
+  def station_route(route)
+    @rsr ||= railway_stations_routes.where(route: route).first
+  end
 end
