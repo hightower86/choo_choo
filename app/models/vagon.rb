f93b7cf2943f92f8f0723794255d9c9ae64dcdb5
Adel <htwr86@gmail.com>
2018-08-14 14:52:10 +0300

    before skype meeting

--- app/models/vagon.rb
+++ app/models/vagon.rb
@@ -1,7 +1,12 @@
 class Vagon < ApplicationRecord
   belongs_to :train
   validates :number, presence: true
-  V_TYPES = ['плацкартный', 'купейный']
+  V_TYPES = [SeatVagon, EconomyVagon, CoupeVagon, ComfortVagon]
+
+  scope :economy, -> { where(type: 'EconomyVagon') }
+  scope :coupe,   -> { where(type: 'CoupeVagon') }
+  scope :comfort, -> { where(type: 'ComforyVagon') }
+  scope :seat,    -> { where(type: 'SeatVagon') }
 
   before_validation :set_number
 
