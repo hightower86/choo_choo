fbcabe53d740650150d78ce1ec53073429161901
Adel <htwr86@gmail.com>
2018-08-28 12:28:56 +0300

    find trains

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,15 +1,25 @@
 class SearchesController < ApplicationController
   
   def show
-    @end_station   = RailwayStation.find(params['end_station'])
+    @end_station   = RailwayStation.find(params[:end_station])
     @start_station = RailwayStation.find(params['start_station'])
-    @routes = Route.railway_stations.find([@start_station, @end_station])
+    @routes = Search.find_routes(@start_station, @end_station)
+    @trains = Search.find_trains(@routes)
   end
-
   def new
     @stations = RailwayStation.all
   end
 
-  private
+  def create
+    # @search = Search.new(search_params)
+    # @end_station   = RailwayStation.find(params[:end_station])
+    # @start_station = RailwayStation.find(params['start_station'])
+    # @routes = Search.find_routes([@start_station, @end_station])
+    # render :show
+  end
 
+  private
+  def search_params
+    params.require(:search).permit(:start_station, :end_station)
+  end
 end
--- app/models/search.rb
+++ app/models/search.rb
@@ -1,7 +1,16 @@
 class Search < ApplicationRecord
 
-  def find_routes(start_station, end_station)
-    @routes = Route.find([start_station, end_station])
+  def self.find_routes(start_station, end_station)
+    start_station.inspect 
+    Route.joins(:railway_stations_routes).where(["railway_stations_routes.railway_station_id = ?", start_station] && ["railway_stations_routes.railway_station_id = ?", end_station])
+  end
+
+  def self.find_trains(routes)
+    Train.where(route: routes)
+  end
+
+  def start_station
+    @start_station = RailwayStation.find(params['start_station'])
   end
 
 end
